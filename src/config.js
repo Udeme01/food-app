7b096e4a550928e255f1b27e137a1c79214a5bed
Udeme01 <emmanueluddy2@gmail.com>
2025-12-23 08:24:59 +0100

    changed backend url vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
 	import.meta.env.VITE_API_BASE_URL ||
 	(import.meta.env.MODE === "production"
-		? "https://your-vercel-backend.vercel.app/api"
+		? "https://your-backend-url.vercel.app/api"
 		: "http://localhost:3000");
